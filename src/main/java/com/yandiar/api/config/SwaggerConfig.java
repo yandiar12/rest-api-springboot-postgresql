57a442e36de1b5e9b47e68481170e4aeb33e2ad7
yandiar12 <yandiar.rohman@gmail.com>
2020-11-16 17:34:17 +0700

    setup mode profile

--- src/main/java/com/yandiar/api/config/SwaggerConfig.java
+++ src/main/java/com/yandiar/api/config/SwaggerConfig.java
@@ -19,7 +19,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
-@Profile("dev")
+//@Profile("dev") // if you want using swagger just on development mode
 public class SwaggerConfig {
 
     @Bean
